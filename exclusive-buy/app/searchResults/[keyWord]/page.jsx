72080c79faa617a400b8520760e8b6dc88bc025d
Güven Fazlı <guven.fazli@gmail.com>
2024-06-02 00:35:27 +0300

    Page Navigator added

--- exclusive-buy/app/searchResults/[keyWord]/page.jsx
+++ exclusive-buy/app/searchResults/[keyWord]/page.jsx
@@ -41,6 +41,14 @@ export default function SearchResultPage({ params }) {
     setLoading(false)
   }
 
+  function navigatePage(option) {
+    if (option === '+') {
+      setPage((prev) => prev += 1)
+    } else if (option === '-') {
+      setPage((prev) => prev -= 1)
+    }
+  }
+
   return (
     <div className="p-4 flex w-full">
       <ResultNavBar priceFilter={priceFilter} setPriceFilter={setPriceFilter} isOnSale={isOnSale} setIsOnSale={setIsOnSale} setListFilter={setListFilter} listFilter={listFilter} filterResults={filterResults} condition={condition} setCondition={setCondition} />
@@ -48,23 +56,22 @@ export default function SearchResultPage({ params }) {
 
 
         <div className="flex flex-col w-full">
-          <div className="w-full px-4 flex justify-around items-center mb-4">
+          <div className="w-full px-8 flex justify-between items-center mb-4">
 
-            <div className="flex w-1/6 py-2 border justify-around items-center">
+            <div className="flex w-1/6 py-2 border rounded-2xl justify-around items-center">
               <div className="flex">
-                {!listFilter === 'RELEVANCE' && <p className="text-lg text-black">{listFilter}</p>}
-
+                <p className="text-sm text-black">{(listFilter === 'RELEVANCE' && 'Relevance') || (listFilter === 'LOWEST_PRICE' && 'Cheap to Expensive') || (listFilter === 'HIGHEST_PRICE' && 'Expensive to Cheap') || (listFilter === 'BEST_SELLERS' && 'Best Sellers')}</p>
               </div>
 
               <div className="flex">
-                <p className="text-lg">{condition}</p>
+                <p className="text-sm">{condition}</p>
               </div>
             </div>
 
             <div className="flex w-1/6 justify-around items-center">
-              <button className="p-1 bg-red-600 rounded-full disabled:bg-red-400">{prevArrow}</button>
+              <button onClick={(() => navigatePage('-'))} disabled={page === 1} className="p-1 bg-red-600 rounded-full disabled:bg-red-400">{prevArrow}</button>
               <p>{page}</p>
-              <button className="p-1 bg-red-600 rounded-full disabled:bg-red-400">{nextArrow}</button>
+              <button onClick={(() => navigatePage('+'))} className="p-1 bg-red-600 rounded-full disabled:bg-red-400">{nextArrow}</button>
             </div>
 
           </div>
