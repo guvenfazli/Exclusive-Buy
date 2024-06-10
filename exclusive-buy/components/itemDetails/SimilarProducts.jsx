fdea1a338f947915c4e94a4784b38c847cff8869
Güven Fazlı <guven.fazli@gmail.com>
2024-06-11 01:26:36 +0300

    Responsive for item detail page is done

--- exclusive-buy/components/itemDetails/SimilarProducts.jsx
+++ exclusive-buy/components/itemDetails/SimilarProducts.jsx
@@ -26,16 +26,16 @@ export default function SimilarProducts({ similarItems }) {
 
   if (product) {
     return (
-      <div className="flex flex-col w-1/4 border items-center justify-around p-4 relative hover:border-red-400 hover:shadow-red-400">
+      <div className="flex flex-col w-1/4 border items-center justify-around p-4 relative hover:border-red-400 hover:shadow-red-400 max-md:w-1/2 max-sm:w-full">
         <Image src={product?.product_photo} width={200} height={200} style={{ objectFit: 'contain' }} alt="Product Image" loading="lazy" />
-        <Link href={`/${product?.product_asin || product?.asin}`} className="text-black line-clamp-3 text-lg mb-4 cursor-pointer hover:underline">{product?.product_title}</Link>
+        <Link href={`/${product?.product_asin || product?.asin}`} className="text-black line-clamp-3 text-lg mb-4 cursor-pointer hover:underline max-sm:text-sm">{product?.product_title}</Link>
         <div className="flex w-full items-center mb-4 justify-around">
-          <p className=" text-red-700 text-lg">{product?.product_price} $</p>
-          <p className="text-gray-500">{product?.product_star_rating} <span className="text-gray-500 text-xs">{'(' + product?.product_num_ratings + ')'}</span></p>
+          <p className=" text-red-700 text-lg max-sm:text-sm">{product?.product_price} $</p>
+          <p className="text-gray-500 max-sm:text-sm">{product?.product_star_rating} <span className="text-gray-500 text-xs">{'(' + product?.product_num_ratings + ')'}</span></p>
           <RatingStars rating={categoryRating} />
         </div>
         <div className="flex w-full justify-center items-center">
-          <p className="mr-2">Best Seller: </p>
+          <p className="mr-2 max-sm:text-sm">Best Seller: </p>
           {product?.is_best_seller ? bestSelling : notBestSelling}
         </div>
       </div>
--- exclusive-buy/components/itemDetails/itemInfo.jsx
+++ exclusive-buy/components/itemDetails/itemInfo.jsx
@@ -86,30 +86,30 @@ export default function ItemInfo({ itemId }) {
     <div className="w-full">
       {loading ? <Loading /> :
         <>
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="flex w-full text-lg justify-around h-max p-4 items-start">
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="flex w-full text-lg justify-around h-max p-4 items-start max-md:flex-col max-md:items-center">
 
-            <div className="relative w-1/5 h-96">
+            <div className="relative w-1/5 h-96 max-md:w-full">
               {<Image src={itemPhotos && itemPhotos[imgIndex]} fill style={{ objectFit: 'contain' }} loading="lazy" alt="Product Image" />}
               <div className="absolute flex bottom-0 w-full justify-around items-center">
                 {itemPhotos?.map((img, index) => <button key={img} onClick={() => changeItemPhoto(index)} className="px-2 py-2 border-4 border-red-700 rounded-3xl bg-white"></button>)}
               </div>
             </div>
 
-            <div className="flex flex-col justify-around w-1/2 items-start p-4">
+            <div className="flex flex-col justify-around w-1/2 items-start p-4 max-md:w-full">
               <div className="flex justify-start items-start mb-2">
-                <p className="font-bold">{product?.product_title}</p>
+                <p className="font-bold max-sm:text-sm">{product?.product_title}</p>
               </div>
 
               <div className="flex justify-start items-center mb-2">
-                <p className="mr-5"><span className="font-bold mr-2 text-red-700">Rating:</span>{product?.product_star_rating} / 5 <span className="text-xs">{`(${product?.product_num_ratings})`}</span></p>
+                <p className="mr-5 max-sm:text-sm"><span className="font-bold mr-2 text-red-700">Rating:</span>{product?.product_star_rating} / 5 <span className="text-xs">{`(${product?.product_num_ratings})`}</span></p>
                 <RatingStars rating={categoryRating} />
               </div>
 
 
               <div className="flex flex-col justify-start items-start mb-2">
-                <p className="text-xl font-bold mb-2 text-red-700">Product Specs</p>
+                <p className="text-xl font-bold mb-2 text-red-700 max-sm:text-sm">Product Specs</p>
                 <div>
-                  {product?.about_product.map((row) => <p key={row} className="mb-2">{row}</p>)}
+                  {product?.about_product.map((row) => <p key={row} className="mb-2 max-sm:text-sm">{row}</p>)}
                 </div>
               </div>
 
@@ -117,37 +117,37 @@ export default function ItemInfo({ itemId }) {
 
               {itemSpecs?.length > 0 &&
                 <div className="flex flex-col justify-start items-start mb-4">
-                  <p className="text-xl font-bold mb-2 text-red-700">Description</p>
+                  <p className="text-xl font-bold mb-2 text-red-700 max-sm:text-sm">Description</p>
 
                   <div>
-                    {itemSpecs?.map((spec, index) => <p key={index} className="mb-2">{spec[0]}: {spec[1]}</p>)}
+                    {itemSpecs?.map((spec, index) => <p key={index} className="mb-2 max-sm:text-sm">{spec[0]}: {spec[1]}</p>)}
                   </div>
                 </div>}
 
 
               <div className="flex flex-col justify-between items-start mb-4">
                 {product?.customers_say && <div>
-                  <p className="text-xl font-bold mb-2 text-red-700">Customer Overall Review</p>
+                  <p className="text-xl font-bold mb-2 text-red-700 max-sm:text-sm">Customer Overall Review</p>
                   <p>{product?.customers_say}</p>
                 </div>}
 
               </div>
             </div>
 
-            <div className="flex flex-col justify-around border h-80 w-1/6 items-center p-4">
-              <p className="text-xl font-bold text-red-700">{product?.product_price + '$'}</p>
-              <p className="text-sm line-through text-gray-600">{product?.product_original_price}</p>
+            <div className="flex flex-col justify-around border h-80 w-1/6 items-center p-4 max-md:w-full">
+              <p className="text-xl font-bold text-red-700 max-sm:text-sm">{product?.product_price + '$'}</p>
+              <p className="text-sm line-through text-gray-600 max-sm:text-sm">{product?.product_original_price}</p>
               <div className="flex items-center">
                 <p className="mr-2 text-sm">Best Selling:</p>
                 {product?.is_best_seller ? bestSelling : notBestSelling}
               </div>
-              <p className={`text-green-700`}>{product?.product_availability ? product?.product_availability : 'In Stock'}</p>
-              <button onClick={() => addToCart()} className="bg-red-700 rounded-2xl text-white p-2 w-full ease-in-out duration-100 hover:bg-red-800">Add to Cart</button>
-              <button onClick={() => addToWishList()} disabled={alreadyInWish} className="bg-red-700 rounded-2xl text-white p-2 w-full ease-in-out duration-100 hover:bg-red-800 disabled:bg-green-200">Add to Wishlist</button>
+              <p className={`text-green-700 max-sm:text-sm`}>{product?.product_availability ? product?.product_availability : 'In Stock'}</p>
+              <button onClick={() => addToCart()} className="bg-red-700 rounded-2xl text-white p-2 w-full ease-in-out duration-100 hover:bg-red-800 max-sm:text-sm">Add to Cart</button>
+              <button onClick={() => addToWishList()} disabled={alreadyInWish} className="bg-red-700 rounded-2xl text-white p-2 w-full ease-in-out duration-100 hover:bg-red-800 disabled:bg-green-200 max-sm:text-sm">Add to Wishlist</button>
             </div>
           </motion.div>
           <div className="w-full mb-4 flex justify-center items-center">
-            <p className="text-2xl text-red-700 font-bold">Users Also Searched For</p>
+            <p className="text-2xl text-red-700 font-bold max-sm:text-sm">Users Also Searched For</p>
           </div>
           <div className="flex w-full gap-y-4 gap-x-4 flex-wrap justify-around items-start">
             {similarItems ? similarItems.map((item) => <SimilarProducts key={item.asin} similarItems={item} />) : <p>No Similar Products Found.</p>}
